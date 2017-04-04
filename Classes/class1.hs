e18e58513247ae85d5a8181af0b67a5a91b33323
AndyT94 <andytruong94@gmail.com>
2017-04-04 16:53:03 +0200

    rm spaces

--- Classes/class1.hs
+++ Classes/class1.hs
@@ -1,16 +1,16 @@
 -- The Maximum Function
 maxi :: Ord a => a -> a -> a
 maxi x y
-  | x >= y    = x
+  | x >= y = x
   | otherwise = y
 
 
 -- Sum of squares recursion
 sumsq :: Int -> Int
 sumsq n
-  | n <= 0    = 0
+  | n <= 0 = 0
   | otherwise = n * n + sumsq (n-1)
 
 
 -- Sum of squares mapping
-sumsqMap :: Int -> Int 
+sumsqMap :: Int -> Int
