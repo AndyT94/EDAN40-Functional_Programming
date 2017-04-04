a8ec3200cf7079f47309ad2900f97dd9737a2d9c
AndyT94 <andytruong94@gmail.com>
2017-04-04 16:52:16 +0200

    sum square recursion done

--- Classes/class1.hs
+++ Classes/class1.hs
@@ -3,3 +3,14 @@ maxi :: Ord a => a -> a -> a
 maxi x y
   | x >= y    = x
   | otherwise = y
+
+
+-- Sum of squares recursion
+sumsq :: Int -> Int
+sumsq n
+  | n <= 0    = 0
+  | otherwise = n * n + sumsq (n-1)
+
+
+-- Sum of squares mapping
+sumsqMap :: Int -> Int 
