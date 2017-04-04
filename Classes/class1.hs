75403682e2587262b6fda8d413b60cd49629112a
AndyT94 <andytruong94@gmail.com>
2017-04-04 22:10:37 +0200

    pythagorean

--- Classes/class1.hs
+++ Classes/class1.hs
@@ -82,8 +82,10 @@ removeDuplicates (x:xs)
   | elem x xs = removeDuplicates xs
   | otherwise = x: removeDuplicates xs
 
-prop_duplicatesRemoved :: [Integer] -> Bool
+prop_duplicatesRemoved :: [Int] -> Bool
 prop_duplicatesRemoved xs = not (duplicates (removeDuplicates xs))
 
 
 -- Comprehensions
+pythagorean :: Int -> [(Int, Int, Int)]
+pythagorean n = [(a, b, c) | a <- [1..n], b <- [1..n], c <- [1..n], a^2 + b^2 == c^2]
