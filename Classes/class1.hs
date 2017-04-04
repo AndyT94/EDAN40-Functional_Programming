5e8a345b84eb7890847c0a33e685e0475e950b6b
AndyT94 <andytruong94@gmail.com>
2017-04-04 20:17:24 +0200

    factors

--- Classes/class1.hs
+++ Classes/class1.hs
@@ -24,3 +24,17 @@ hanoi :: Int -> Int
 hanoi n
   | n <= 0 = 0
   | otherwise = 1 + 2 * hanoi (n - 1)
+
+
+-- Factors
+smallestFactor :: Int -> Int
+smallestFactor n = nextFactor 2 n
+
+nextFactor :: Int -> Int -> Int
+nextFactor k n
+  | k >= n = n
+  | mod n k == 0 = k
+  | otherwise = nextFactor (k+1) n
+
+numFactors :: Int -> Int
+numFactors n = length [x | x <- [1..n], mod n x == 0]
