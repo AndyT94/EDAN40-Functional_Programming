f8818eedcb24822366886c2c6276495018a0d436
AndyT94 <andytruong94@gmail.com>
2017-05-09 22:45:49 +0200

    type derivation

--- Classes/class2.hs
+++ Classes/class2.hs
@@ -32,3 +32,29 @@ allVals (x:xs) = [ (x,b):val | val <- allVals xs, b <- [False,True] ]
 --   appearing in it
 tautology :: Proposition -> Bool
 tautology a = and [ truthValue a val | val <- allVals (vars a) ]
+
+
+-- 2.6 Type derivation
+(.)(:) :: (a1 -> a) -> a1 -> [a] -> [a]
+
+* Couldn't match expected type `[a]'
+              with actual type `(b0 -> c0) -> (a0 -> b0) -> a0 -> c0'
+* Probable cause: `(.)' is applied to too few arguments
+  In the second argument of `(:)', namely `(.)'
+  In the expression: (: (.))
+
+((.):)
+  :: [(b -> c) -> (a -> b) -> a -> c]
+     -> [(b -> c) -> (a -> b) -> a -> c]
+
+((:):) :: [a -> [a] -> [a]] -> [a -> [a] -> [a]]
+
+(.)(.) :: (a1 -> b -> c) -> a1 -> (a -> b) -> a -> c
+
+(8-) :: Num a => a -> a
+
+(+0).(0+) :: Num c => c -> c
+
+(($)$($)) :: (a -> b) -> a -> b
+
+([]>>=)(\_->[(>=)]) :: Ord a => [a -> a -> Bool]
